00b675cbb6761bf49209ea61dcb456f53be3e230
Guilherme M. P. Silva <gmprestes@gmail.com>
2015-08-04 17:16:50 -0300

    Commit

--- html/uni.js
+++ html/uni.js
@@ -10,10 +10,15 @@ var page = require('webpage').create(),
   fs = require('fs'),
   system = require('system');
 
+
+page.viewportSize = {
+  width: 1280,
+  height: 800
+};
 console.log('The default user agent is ' + page.settings.userAgent);
 
 page.open('https://portal.asav.org.br/corpore.net/Login.aspx%3FReturnUrl%3D%2Fcorpore.net%2FMain.aspx%3FSelectedMenuIDKey%3DmnQuadroHorario%26ActionID%3DEduQuadroHorarioAlunoActionWeb', function(status) {
-//page.open('http://minha.unisinos.br', function(status) {
+  //page.open('http://minha.unisinos.br', function(status) {
 
   if (status !== 'success') {
     console.log('Unable to load the address!');
@@ -30,18 +35,28 @@ page.open('https://portal.asav.org.br/corpore.net/Login.aspx%3FReturnUrl%3D%2Fco
         //document.getElementById("form-login").submit();
       });
 
-      var i = 1;
-      while (i < 10000000) {
+      /*var i = 1;
+      while (i < 10000000000) {
         i++;
-      }
-      //console.log(page.content);
-      var content = page.content;
-      page.render('export.png');
-      fs.write('1.html', content, 'w');
-      phantom.exit();
+      }*/
+
+      window.setTimeout(function() {
+        page.evaluate(function() {
+
+          document.getElementById('grid_ctl07_grid:RMWSelectTemplate').click();
+        });
+        window.setTimeout(function() {
+
+          //console.log(page.content);
+          var content = page.content;
+          page.render('export.png');
+          fs.write('1.html', content, 'w');
+          phantom.exit();
+        }, 5000);
+      }, 10000);
 
 
-      phantom.exit();
+      //  phantom.exit();
     }, 1000);
   }
 });
