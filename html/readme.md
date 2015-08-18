daf6c8a3bc34d0224391e2d3558c2c062fd29b5a
Guilherme M. P. Silva <gmprestes@gmail.com>
2015-08-18 12:09:12 -0300

    Clique no modal de contexto

--- html/readme.md
+++ html/readme.md
@@ -16,3 +16,17 @@ console.log(element);
 var iframe = $(".dxpcContent iframe");
 var element = iframe.find($('#grid_ctl07_grid\\:RMWSelectTemplate'));
 console.log(element);
+
+
+xpcContexto.Show();
+
+var iframe = document.getElementsByTagName("iframe")[0];
+
+var doc = iframe.contentWindow.document;
+
+var trContexto2015 = doc.getElementById('grid_ctl07_grid\:RMWSelectTemplate').parentNode.parentNode;
+
+var el = trContexto2015.getElementsByTagName("td")[1].firstChild;
+el.click();
+
+var tableDisciplinas = document.getElementById("ctl23_gvDisciplinas");
--- html/uni.js
+++ html/uni.js
@@ -1,62 +1,66 @@
-function escapeRegExp(str) {
-  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-}
+try {
+  function escapeRegExp(str) {
+    return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+  }
 
-function replaceAll(find, replace, str) {
-  return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
-}
+  function replaceAll(find, replace, str) {
+    return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+  }
 
-var page = require('webpage').create(),
-  fs = require('fs'),
-  system = require('system');
+  var page = require('webpage').create(),
+    fs = require('fs'),
+    system = require('system');
 
 
-page.viewportSize = {
-  width: 1280,
-  height: 800
-};
-console.log('The default user agent is ' + page.settings.userAgent);
+  page.viewportSize = {
+    width: 1280,
+    height: 800
+  };
+  console.log('The default user agent is ' + page.settings.userAgent);
 
-page.open('https://portal.asav.org.br/corpore.net/Login.aspx%3FReturnUrl%3D%2Fcorpore.net%2FMain.aspx%3FSelectedMenuIDKey%3DmnQuadroHorario%26ActionID%3DEduQuadroHorarioAlunoActionWeb', function(status) {
-  //page.open('http://minha.unisinos.br', function(status) {
+  page.open('https://portal.asav.org.br/corpore.net/Login.aspx%3FReturnUrl%3D%2Fcorpore.net%2FMain.aspx%3FSelectedMenuIDKey%3DmnQuadroHorario%26ActionID%3DEduQuadroHorarioAlunoActionWeb', function(status) {
+    //page.open('http://minha.unisinos.br', function(status) {
+    if (status !== 'success') {
+      console.log('Unable to load the address!');
+      console.log(status);
 
-  if (status !== 'success') {
-    console.log('Unable to load the address!');
-    console.log(status);
-
-    phantom.exit(1);
-  } else {
-
-    window.setTimeout(function() {
-      page.evaluate(function() {
-        document.getElementById('txtUser').value = "gmprestes";
-        document.getElementById('txtPass').value = "pinkcf7500000";
-        document.getElementById('btnLogin').click();
-        //document.getElementById("form-login").submit();
-      });
-
-      /*var i = 1;
-      while (i < 10000000000) {
-        i++;
-      }*/
+      phantom.exit(1);
+    } else {
 
       window.setTimeout(function() {
-        page.evaluate(function() {
 
-          document.getElementById('grid_ctl07_grid\\:RMWSelectTemplate').click();
-        });
+
+        // Sessão OK
+        page.evaluate(function(user, pass) {
+          document.getElementById('txtUser').value = user; //"gmprestes";
+          document.getElementById('txtPass').value = pass; // "pinkcf7500000";
+          document.getElementById('btnLogin').click();
+        }, system.args[1], system.args[2]);
+
         window.setTimeout(function() {
+          // clica no modal de contexto;
+          page.evaluate(function() {
+            //xpcContexto.Show();
+            var iframe = document.getElementsByTagName("iframe")[0];
+            var doc = iframe.contentWindow.document;
+            var trContexto2015 = doc.getElementById('grid_ctl07_grid\:RMWSelectTemplate').parentNode.parentNode;
+            var el = trContexto2015.getElementsByTagName("td")[1].firstChild;
+            el.click();
+          });
 
-          //console.log(page.content);
-          var content = page.content;
-          page.render('export.png');
-          fs.write('1.html', content, 'w');
-          phantom.exit();
-        }, 5000);
-      }, 10000);
-
-
-      //  phantom.exit();
-    }, 1000);
-  }
-});
+          window.setTimeout(function() {
+            var content = page.content;
+            //page.render('export.png');
+            fs.write(system.args[3] + '.html', content, 'w');
+            console.log("OK");
+            phantom.exit(1);
+          }, 5000);
+        }, 7000); // segundo timeout
+      }, 1000);
+    }
+  });
+} catch (Erro) {
+  console.log(Erro);
+  page.render(system.args[3] + '.png');
+  phantom.exit();
+}
